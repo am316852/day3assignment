56a422f3277c8c223fc824d80280e20294ebfa8c
am316852 <amitkjhajharia@gmail.com>
2016-05-16 19:34:03 +0530

    second

---
+++ day3/includes/js/script.js
@@ -0,0 +1,17 @@
+/*
+
+My Custom JS
+============
+
+Author:  Amit kumar
+Updated: may 2016
+Notes:	 Hand coded by amit kumar
+
+*/
+$(function(){
+  $('#alertMe').click(function(e){
+    e.preventDefault();
+    $('#successAlert').slideDown();
+  });
+
+});
